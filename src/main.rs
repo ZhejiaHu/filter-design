5bcfe21d8681e5dbe70300862cdcf653c09184ea
Zhejia Hu <z.hu-3@student.tudelft.nl>
2025-03-26 22:46:19 +0100

    add testbench for filter

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
+use std::option::Option;
 use core::{
     ops::{ Add, AddAssign, Mul, MulAssign, Div, DivAssign, Neg  },
     cmp::{ PartialEq, PartialOrd }
 };
+use std::{ error::Error, path::Path};
+use std::fs::File;
 use fixed::types::I18F14;
+use csv::{Reader, Writer};
+use serde::{Deserialize, Serialize};
 
 pub trait OneZero {
     fn one() -> Self;
     fn zero() -> Self;
     fn abs(self) -> Self;
+
+    fn to_f32(self) -> f32;
+
     fn from_usize(num_u: usize) -> Self;
     fn from_f32(num_f: f32) -> Self;
 }
@@ -16,6 +24,9 @@ impl OneZero for f32 {
     fn one() -> Self { 1.0 }
     fn zero() -> Self { 0.0 }
     fn abs(self) -> Self { self.abs() }
+
+    fn to_f32(self) -> f32 { self }
+
     fn from_usize(num_u: usize) -> Self { num_u as f32 }
 
     fn from_f32(num_f: f32) -> Self { num_f }
@@ -26,6 +37,8 @@ impl OneZero for I18F14 {
 
     fn zero() -> Self { I18F14::from_num(0.0) }
     fn abs(self) -> Self { self.abs() }
+
+    fn to_f32(self) -> f32 { self.to_num() }
 
     fn from_usize(num_u: usize) -> Self { I18F14::from_num(num_u) }
     fn from_f32(num_f: f32) -> Self { I18F14::from_num(num_f) }
@@ -140,7 +153,7 @@ where T: Add<Output = T> + AddAssign + Mul<Output = T> + MulAssign + Div<Output 
 
 
 impl<T> KalmanFilter<T>
-where T: Add<Output = T> + AddAssign + Mul<Output = T> + MulAssign + Div<Output = T> + DivAssign + Neg<Output = T> + Copy + PartialEq + PartialOrd + OneZero{
+where T: Add<Output = T> + AddAssign + Mul<Output = T> + MulAssign + Div<Output = T> + DivAssign + Neg<Output = T> + Copy + PartialEq + PartialOrd + OneZero {
     pub(crate) fn new(c1: T, c2: T, p2phi: T) -> Self {
         KalmanFilter { c1, c2, p2phi, bias: T::zero(), phi: T::zero() }
     }
@@ -155,4 +168,82 @@ where T: Add<Output = T> + AddAssign + Mul<Output = T> + MulAssign + Div<Output 
     }
 }
 
-fn main() {}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Data<T>
+where T: Add<Output = T> + AddAssign + Mul<Output = T> + MulAssign + Div<Output = T> + DivAssign + Neg<Output = T> + Copy + PartialEq + PartialOrd + OneZero
+{
+    raw_roll: T, raw_pitch: T, raw_yar: T,
+    raw_x: T, raw_y: T, raw_z: T,
+    dmp_roll: T, dmp_pitch: T, dmp_yaw: T
+}
+
+
+impl<T> Data<T>
+where T: Add<Output = T> + AddAssign + Mul<Output = T> + MulAssign + Div<Output = T> + DivAssign + Neg<Output = T> + Copy + PartialEq + PartialOrd + OneZero {
+    fn from(f_data: Data<f32>) -> Self {
+        Data {
+            raw_roll: T::from_f32(f_data.raw_roll), raw_pitch: T::from_f32(f_data.raw_pitch), raw_yar: T::from_f32(f_data.raw_yar),
+            raw_x: T::from_f32(f_data.raw_x), raw_y: T::from_f32(f_data.raw_y), raw_z: T::from_f32(f_data.raw_z),
+            dmp_roll: T::from_f32(f_data.dmp_roll), dmp_pitch: T::from_f32(f_data.dmp_pitch), dmp_yaw: T::from_f32(f_data.dmp_yaw) }
+    }
+}
+
+
+
+fn system_without_butterworth<T>(
+    theta_slash: T, phi_slash: T, psi_slash: T, x_dd_slash: T, y_dd_slash: T, z_dd_slash: T, degree: Option<usize>, small_angle: Option<bool>,
+    theta_kf_config_: Option<(T, T, T)>, phi_kf_config_: Option<(T, T, T)>, psi_kf_config_: Option<(T, T, T)>) -> (T, T, T)
+where T: Add<Output = T> + AddAssign + Mul<Output = T> + MulAssign + Div<Output = T> + DivAssign + Neg<Output = T> + Copy + PartialEq + PartialOrd + OneZero {
+    let (theta_kf_config, phi_kf_config, psi_kf_config): ((T, T, T), (T, T, T), (T, T, T)) = (
+        theta_kf_config_.unwrap_or((T::zero(), T::zero(), T::zero())), phi_kf_config_.unwrap_or((T::zero(), T::zero(), T::zero())), psi_kf_config_.unwrap_or((T::zero(), T::zero(), T::zero()))
+    );
+    let (theta_tilde, phi_tilde): (T, T) = row_pitch(x_dd_slash, y_dd_slash, z_dd_slash, degree, small_angle);
+    let (mut kalman_theta, mut kalman_phi, mut kalman_psi): (KalmanFilter<T>, KalmanFilter<T>, KalmanFilter<T>) = (
+        KalmanFilter::new(theta_kf_config.0, theta_kf_config.1, theta_kf_config.2),
+        KalmanFilter::new(phi_kf_config.0, phi_kf_config.1, phi_kf_config.2),
+        KalmanFilter::new(psi_kf_config.0, psi_kf_config.1, psi_kf_config.2)
+    );
+    (
+        kalman_theta.predict(theta_slash, theta_tilde).0,
+        kalman_phi.predict(phi_slash, phi_tilde).0,
+        psi_slash
+    )
+}
+
+
+fn process_data<T>(dataset: &Path) -> Result<Vec<Data<T>>, Box<dyn Error>>
+where T: Add<Output = T> + AddAssign + Mul<Output = T> + MulAssign + Div<Output = T> + DivAssign + Neg<Output = T> + Copy + PartialEq + PartialOrd + OneZero {
+    let mut reader = csv::Reader::from_path(dataset)?;
+    let mut data_vec: Vec<Data<T>> = Vec::new();
+    for result in reader.deserialize() {
+        let data_raw: Data<f32> = result?;
+        data_vec.push(Data::from(data_raw));
+    }
+    Ok(data_vec)
+}
+
+
+
+fn testbench_data<T>(dataset: &Path, output_path: &Path, row_kf_config: Option<(T, T, T)>, pitch_kf_config: Option<(T, T, T)>, yaw_kf_config: Option<(T, T, T)>)
+where T: Add<Output = T> + AddAssign + Mul<Output = T> + MulAssign + Div<Output = T> + DivAssign + Neg<Output = T> + Copy + PartialEq + PartialOrd + OneZero {
+    let dataset: Vec<Data<T>> = process_data(dataset).unwrap();
+    let mut writer = Writer::from_path(output_path).unwrap();
+    for data in dataset {
+        let (filtered_roll, filtered_pitch, filtered_raw): (T, T, T) = system_without_butterworth(
+            data.raw_roll, data.dmp_pitch, data.dmp_yaw, data.raw_x, data.raw_y, data.raw_z,
+            Some(10), Some(false), row_kf_config, pitch_kf_config, yaw_kf_config
+        );
+        writer.write_record(&[
+            filtered_roll.to_f32().to_string(),
+            filtered_pitch.to_f32().to_string(),
+            filtered_roll.to_f32().to_string()]
+        ).unwrap();
+    }
+}
+
+
+
+fn main() {
+
+}
